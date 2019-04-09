8d9b6e4c5d2a8d502bd6eef3da43986f5632cfea
DS1R3S <dsires13@mac.com>
2019-04-09 15:19:09 -0400

    Updates

---
+++ precache-manifest.60c7d650511912cb329044f8add376f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b92d51dc88304c348c9",
+    "url": "/clicky-game-react/static/css/main.1a0bc5e5.chunk.css"
+  },
+  {
+    "revision": "0b92d51dc88304c348c9",
+    "url": "/clicky-game-react/static/js/main.63ffecdb.chunk.js"
+  },
+  {
+    "revision": "96e9a9b550dae971e8fe",
+    "url": "/clicky-game-react/static/js/runtime~main.4ef103c0.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game-react/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "e89fecb67afe2906d255402ca22aea9f",
+    "url": "/clicky-game-react/index.html"
+  }
+];
